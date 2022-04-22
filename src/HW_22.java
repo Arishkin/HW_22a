7aaf1f87b036b9809874d5b759f771b940167a22
Arishkin <arina.karlina@gmail.com>
2022-04-22 10:53:09 +0300

    Initial commit

---
+++ src/HW_22.java
@@ -0,0 +1,12 @@
+public class HW_22 {
+    public static void main(String[] args){
+
+        String myNameInSlack = "Arinushka";
+
+
+        System.out.println("In Slack I am " + " " + myNameInSlack);
+
+
+
+    }
+}
